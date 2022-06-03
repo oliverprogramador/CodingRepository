d5c0a5d8d82024ee7bdf0412e762cc8031ee3dd2
moises <106493333+oliverprogramador@users.noreply.github.com>
2022-06-02 21:51:07 -0300

    first commit

---
+++ Alura-JavaScript-Arrays/splice.js
@@ -0,0 +1,7 @@
+// .splice atualiza um array removendo um indice e colocando outro 
+
+const ListadeMendigos = ['Pedro', 'David', 'Will', 'Thomas']
+
+ListadeMendigos.splice(0,1, 'Moisés')
+
+console.log(`Os mendigos são: ${ListadeMendigos}`)
