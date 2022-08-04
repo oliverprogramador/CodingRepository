d86e6ab7ca4fd3d9401773be1a67a8f85b1f6f9d
moises <106493333+oliverprogramador@users.noreply.github.com>
2022-08-04 17:39:55 -0300

    comparacoes.js

--- Alura-JavaScript/comparacoes.js
+++ Alura-JavaScript/comparacoes.js
@@ -1,2 +1,3 @@
 // comparação implicita(==): compara o valor e converte string em numero ou vice e versa
 // comparação explicita(===): compara o valor e o tipo de dado mas não converte
+//afafassassa
